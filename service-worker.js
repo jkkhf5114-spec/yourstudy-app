1527275af61a67d828e7ce18d0ce2af364499324
jkkhf5114-spec <jkkhf5114@gmail.com>
2025-12-20 20:10:50 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
-// Updated Version to v5 for Force Update
-const CACHE_NAME = 'yourstudy-v5';
-const urlsToCache = [
-  'index.html',
-  'manifest.json',
-  'study.png'
-];
+const CACHE_NAME = 'yourstudy-v6';
+const urlsToCache = ['index.html', 'manifest.json', 'study.png'];
 
 self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) return caches.delete(cache);
+        })
+      );
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
